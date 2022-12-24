23b7fc2aac7ab2896889de9f6aa99727b2fa6298
Nathan Thomas <nwthomas@me.com>
2022-12-23 23:05:08 -0800

    Revert helmet version for use in demo server

--- api/middleware.js
+++ api/middleware.js
@@ -5,5 +5,5 @@ const cors = require("cors");
 module.exports = (server) => {
   server.use(express.json());
   server.use(cors());
-  server.use(helmet());
+  server.use(helmet({ contentSecurityPolicy: false }));
 };
